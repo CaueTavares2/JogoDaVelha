1c916229f3bbe799793cf3bcdb21e2240276d4cd
Dimasu CP <dimasu-cp@hotmail.com>
2025-09-26 16:22:31 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+using System;
+
+namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.White;
+            this.pb1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb1.Location = new System.Drawing.Point(260, 51);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(100, 100);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.DimGray;
+            this.lblStatus.Location = new System.Drawing.Point(53, 62);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(32, 15);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "label";
+            // 
+            // pb4
+            // 
+            this.pb4.BackColor = System.Drawing.Color.White;
+            this.pb4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb4.Location = new System.Drawing.Point(260, 157);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(100, 100);
+            this.pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb4.TabIndex = 2;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.BackColor = System.Drawing.Color.White;
+            this.pb7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb7.Location = new System.Drawing.Point(260, 263);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(100, 100);
+            this.pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb7.TabIndex = 3;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackColor = System.Drawing.Color.White;
+            this.pb2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb2.Location = new System.Drawing.Point(366, 51);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(100, 100);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 4;
+            this.pb2.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.BackColor = System.Drawing.Color.White;
+            this.pb5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb5.Location = new System.Drawing.Point(366, 157);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(100, 100);
+            this.pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb5.TabIndex = 5;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.BackColor = System.Drawing.Color.White;
+            this.pb8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb8.Location = new System.Drawing.Point(366, 263);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(100, 100);
+            this.pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb8.TabIndex = 6;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.BackColor = System.Drawing.Color.White;
+            this.pb3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb3.Location = new System.Drawing.Point(472, 51);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(100, 100);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb3.TabIndex = 7;
+            this.pb3.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.BackColor = System.Drawing.Color.White;
+            this.pb6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb6.Location = new System.Drawing.Point(472, 157);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(100, 100);
+            this.pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb6.TabIndex = 8;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb9
+            // 
+            this.pb9.BackColor = System.Drawing.Color.White;
+            this.pb9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb9.Location = new System.Drawing.Point(472, 263);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(100, 100);
+            this.pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb9.TabIndex = 9;
+            this.pb9.TabStop = false;
+            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(391, 396);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Resetar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "\\";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using JogoDaVelha.Properties; // Importa os recursos (imagens X e O)
+
+namespace JogoDaVelha
+{
+    // A classe principal do seu formulário
+    public partial class Form1 : Form
+    {
+        // 1. Variáveis de Estado (Matriz de Dados e Controle)
+        private string[,] MatrizDeEstado = new string[3, 3];
+        // X agora é DarkViolet, O é LimeGreen
+        private bool VezDoX = true;
+        private bool JogoFinalizado = false;
+        private int ContadorDeJogadas = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Chamadas de inicialização
+            ConfigurarEstiloEEventos();
+            InicializarTabuleiro();
+        }
+
+        // --- Configuração e Inicialização ---
+
+        private void ConfigurarEstiloEEventos()
+        {
+            // Aplica o esquema de cores Moderno/Tech no formulário principal
+            this.BackColor = Color.Gainsboro;
+
+            // Itera sobre todos os controles no formulário para configurar as PictureBoxes
+            foreach (Control controle in this.Controls)
+            {
+                // Verifica se é uma PictureBox e se o nome segue o novo padrão 'pb' + número
+                if (controle is PictureBox && controle.Name.StartsWith("pb") && controle.Name.Length <= 4)
+                {
+                    PictureBox pb = (PictureBox)controle;
+                    // NOTA SOBRE CS1061: A linha abaixo garante que todas as PictureBoxes usem o mesmo método.
+                    // Se você está vendo CS1061, vá para o Form1.Designer.cs (ou a janela de Eventos) 
+                    // e certifique-se de que o evento 'Click' de cada PictureBox (pb1, pb2, etc.)
+                    // esteja definido como 'PictureBox_Click' e NÃO como 'pb1_Click' ou similar.
+                    pb.Click += PictureBox_Click;
+                    pb.BackColor = Color.White; // Fundo da célula
+                    pb.BorderStyle = BorderStyle.FixedSingle; // Borda sutil (DarkGray)
+                }
+            }
+
+            // Associa o evento de clique do botão de reinício (nomeado como btnReset)
+            btnReset.Click += btnReset_Click;
+            lblStatus.ForeColor = Color.DimGray;
+        }
+
+        private void InicializarTabuleiro()
+        {
+            // Zera a matriz, o contador e o estado do jogo
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    MatrizDeEstado[i, j] = ""; // Limpa a matriz
+                }
+            }
+
+            // Limpa as imagens visuais em todas as PictureBoxes
+            foreach (Control controle in this.Controls)
+            {
+                // Verifica se é uma PictureBox e se o nome segue o novo padrão 'pb' + número
+                if (controle is PictureBox && controle.Name.StartsWith("pb") && controle.Name.Length <= 4)
+                {
+                    ((PictureBox)controle).Image = null;
+                }
+            }
+
+            VezDoX = true;
+            JogoFinalizado = false;
+            ContadorDeJogadas = 0;
+            lblStatus.Text = "Vez do: X";
+            lblStatus.ForeColor = Color.DimGray;
+        }
+
+
+        // --- Lógica Principal do Jogo (Chamada em cada clique da PictureBox) ---
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            // 0. Condição de Parada
+            if (JogoFinalizado) return;
+
+            PictureBox pbClicada = (PictureBox)sender;
+
+            // 1. Mapeamento: Obtém o número (1 a 9) e converte para (Linha, Coluna)
+            // Novo Mapeamento: pb1 -> 1, pb9 -> 9
+            int numeroControle = int.Parse(pbClicada.Name.Substring(2));
+            (int linha, int coluna) = MapearNumeroParaMatriz(numeroControle);
+
+            // 2. Checar Jogada Válida (célula vazia)
+            if (MatrizDeEstado[linha, coluna] != "")
+            {
+                return;
+            }
+
+            // 3. Determinar Símbolo e Imagem (Cores Moderno/Tech INVERTIDAS)
+            string simbolo;
+            Image imagemSimbolo;
+
+            if (VezDoX)
+            {
+                simbolo = "X";
+                // X (DarkViolet)
+                // CORREÇÃO CS0117: Usando o novo nome de recurso X_DarkViolet
+                imagemSimbolo = Resources.X_DarkViolet;
+            }
+            else
+            {
+                simbolo = "O";
+                // O (LimeGreen)
+                // CORREÇÃO CS0117: Usando o novo nome de recurso O_LimeGreen
+                imagemSimbolo = Resources.O_LimeGreen;
+            }
+
+            // 4. Atualizar Estado (Matriz e UI)
+            MatrizDeEstado[linha, coluna] = simbolo;
+            pbClicada.Image = imagemSimbolo;
+            ContadorDeJogadas++;
+
+            // 5. Verificar Resultado
+            if (VerificarVitoria(simbolo))
+            {
+                lblStatus.Text = $"VENCEDOR: {simbolo}!";
+                // Destaca o vencedor com a cor CORRETA (X=DarkViolet, O=LimeGreen)
+                lblStatus.ForeColor = (simbolo == "X") ? Color.DarkViolet : Color.LimeGreen;
+                JogoFinalizado = true;
+                return;
+            }
+
+            // Verifica empate se todas as 9 jogadas foram feitas sem vencedor
+            if (ContadorDeJogadas == 9)
+            {
+                lblStatus.Text = "EMPATE! (Jogo Finalizado)";
+                JogoFinalizado = true;
+                return;
+            }
+
+            // 6. Trocar a Vez e atualizar o Label
+            VezDoX = !VezDoX;
+            lblStatus.Text = "Vez do: " + (VezDoX ? "X" : "O");
+        }
+
+        /// <summary>
+        /// Converte o número sequencial do controle (1 a 9) para coordenadas (Linha, Coluna) da matriz (0,0 a 2,2).
+        /// </summary>
+        /// <param name="numero">Número do PictureBox (1 a 9).</param>
+        /// <returns>Tupla (Linha, Coluna).</returns>
+        private (int linha, int coluna) MapearNumeroParaMatriz(int numero)
+        {
+            // O mapeamento é: (Número - 1) / 3 para a linha, e (Número - 1) % 3 para a coluna.
+            int index = numero - 1;
+            int linha = index / 3;
+            int coluna = index % 3;
+            return (linha, coluna);
+        }
+
+        private bool VerificarVitoria(string simbolo)
+        {
+            // A. Verifica Linhas e Colunas (3 em linha, 3 em coluna)
+            for (int i = 0; i < 3; i++)
+            {
+                // Linhas
+                if (MatrizDeEstado[i, 0] == simbolo && MatrizDeEstado[i, 1] == simbolo && MatrizDeEstado[i, 2] == simbolo)
+                {
+                    return true;
+                }
+                // Colunas
+                if (MatrizDeEstado[0, i] == simbolo && MatrizDeEstado[1, i] == simbolo && MatrizDeEstado[2, i] == simbolo)
+                {
+                    return true;
+                }
+            }
+
+            // B. Verifica Diagonais
+
+            // Diagonal Principal (Superior Esquerda para Inferior Direita)
+            if (MatrizDeEstado[0, 0] == simbolo && MatrizDeEstado[1, 1] == simbolo && MatrizDeEstado[2, 2] == simbolo)
+            {
+                return true;
+            }
+
+            // Diagonal Secundária (Superior Direita para Inferior Esquerda)
+            if (MatrizDeEstado[0, 2] == simbolo && MatrizDeEstado[1, 1] == simbolo && MatrizDeEstado[2, 0] == simbolo)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // --- Evento do Botão Reset ---
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            InicializarTabuleiro();
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
